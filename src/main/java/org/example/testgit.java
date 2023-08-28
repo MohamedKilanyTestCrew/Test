712778ad36194d73f47cc5081bd836c2cf3023f7
Reem <Reemjamaan1oo@gmail.com>
2023-08-28 12:11:18 +0300

    creat new class

---
+++ src/main/java/org/example/testgit.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class testgit {public static void main(String[] args) {
+
+}
+
+    public static void print(){
+        System.out.println("Hello world!");
+    }
+}
