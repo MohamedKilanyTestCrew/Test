9caeb937207f83f42575e441a995bf1b6f4b7378
Ejaz4715 <ejazmd15@hotmail.com>
2023-08-28 11:46:35 +0300

    This is a test

---
+++ src/main/java/org/example/AutomationTest.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class AutomationTest {
+    public static void main(String[] args) {
+        System.out.println("This is a sample class");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static void print(){
         System.out.println("Hello world!");
 
     }
