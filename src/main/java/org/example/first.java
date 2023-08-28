035c427e9f08d2f408f33287b82f0368c7234904
DoaaNwe <duo_nwe@hotmail.com>
2023-08-28 11:51:08 +0300

    test1

---
+++ src/main/java/org/example/first.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class first {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(){
+        System.out.println("Hello world!");
+
+}
+}
